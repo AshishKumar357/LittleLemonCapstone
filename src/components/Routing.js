081b02fcd5822604e5b707be1be8b5015d8cb0ef
Ashish Kumar <zashu001@gmail.com>
2023-08-11 00:11:07 +0530

    added assets

--- src/components/Routing.js
+++ src/components/Routing.js
@@ -10,11 +10,8 @@ export default function Routing() {
   return (
     <Routes>
       <Route path="/" element={<Homepage />} />
-
       <Route path="/about" element={<About />} />
-
       <Route path="/reservations" element={<Reservation />} />
-
       <Route path="/order" element={<Order />} />
       <Route path="/login" element={<Login />} />
 
